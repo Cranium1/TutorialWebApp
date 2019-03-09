4f36f4a84f73cc828d78e171950d874398eefe30
Cranium1 <42543263+Cranium1@users.noreply.github.com>
2019-03-09 16:47:42 +0100

    Controller and Service setup

---
+++ src/java/service/HelloService.java
@@ -0,0 +1,19 @@
+package service;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Cees-Pieter
+ */
+public class HelloService
+{
+public static String sayHello(String name)
+{
+    return "Hello " + name + "!";
+}
+}
